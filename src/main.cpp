a0a21679a8c0aa00d7f4e9181179819f0c75f351
Kevin Trieu <kevin.trieu5813@gmail.com>
2024-07-17 01:42:27 -0400

    Add basic GLFW window code

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <format>
 #include <iostream>
 
+#include <GLFW/glfw3.h>
+
 int main(int argc, char **argv)
 {
-    std::cout << std::format("Hello world {}!\n", 7);
+    glfwInit();
+
+    GLFWwindow *window = glfwCreateWindow(1080, 720, "ugo-vk", nullptr, nullptr);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    return 0;
 }
