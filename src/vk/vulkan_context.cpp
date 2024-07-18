c4c36041e7a7009202f3d727ed7d061073a79047
Kevin Trieu <kevin.trieu5813@gmail.com>
2024-07-18 00:09:19 -0400

    Add validation layers

--- src/vk/vulkan_context.cpp
+++ src/vk/vulkan_context.cpp
@@ -1,7 +1,10 @@
 #include "vulkan_context.h"
 
 #include <string>
+#include <cstring>
 #include <stdexcept>
+#include <algorithm>
+#include <format>
 
 #include <GLFW/glfw3.h>
 
@@ -16,8 +19,53 @@ std::vector<const char *> VulkanContext::get_required_extensions()
     const char **glfwExtensions;
     glfwExtensions = glfwGetRequiredInstanceExtensions(&numGlfwExtensions);
 
+    std::vector<const char *> required_extensions(glfwExtensions, glfwExtensions + numGlfwExtensions);
+
+    if (this->enable_validation_layers)
+    {
+        required_extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+    }
+
     // Just return the GLFW extension list for now - we may want more later.
-    return std::vector<const char *>(glfwExtensions, glfwExtensions + numGlfwExtensions);
+    return required_extensions;
+}
+
+const std::vector<const char *> REQUIRED_VALIDATION_LAYERS = {
+    "VK_LAYER_KHRONOS_validation",
+};
+
+std::vector<const char *> VulkanContext::get_validation_layers()
+{
+    uint32_t num_layers;
+    vkEnumerateInstanceLayerProperties(&num_layers, nullptr);
+
+    std::vector<VkLayerProperties> available_layers(num_layers);
+    vkEnumerateInstanceLayerProperties(&num_layers, available_layers.data());
+
+    std::vector<const char *>
+        validation_layers;
+
+    for (const char *required_layer : REQUIRED_VALIDATION_LAYERS)
+    {
+        auto found_layer = std::find_if(
+            available_layers.begin(),
+            available_layers.end(),
+            [required_layer](VkLayerProperties layer)
+            {
+                return std::strcmp(layer.layerName, required_layer) == 0;
+            });
+
+        if (found_layer != available_layers.end())
+        {
+            validation_layers.push_back(required_layer);
+        }
+        else
+        {
+            throw std::runtime_error(std::format("Required validation layer {} not found.", required_layer));
+        }
+    }
+
+    return validation_layers;
 }
 
 void VulkanContext::create_instance()
@@ -35,6 +83,13 @@ void VulkanContext::create_instance()
     auto required_extensions = this->get_required_extensions();
     create_info.enabledExtensionCount = required_extensions.size();
     create_info.ppEnabledExtensionNames = required_extensions.data();
+
+    auto validation_layers = this->get_validation_layers();
+    if (this->enable_validation_layers)
+    {
+        create_info.enabledLayerCount = validation_layers.size();
+        create_info.ppEnabledLayerNames = validation_layers.data();
+    }
 
     VkResult create_result = vkCreateInstance(&create_info, nullptr, &this->instance);
     if (create_result != VK_SUCCESS)
--- src/vk/vulkan_context.h
+++ src/vk/vulkan_context.h
@@ -10,8 +10,16 @@ public:
 
 private:
     std::vector<const char *> get_required_extensions();
+    std::vector<const char *> get_validation_layers();
     void create_instance();
 
+#ifdef NDEBUG
+    const bool enable_validation_layers = false;
+#else
+    const bool enable_validation_layers = true;
+#endif
+
     VkInstance instance;
+
     std::string app_name;
 };
